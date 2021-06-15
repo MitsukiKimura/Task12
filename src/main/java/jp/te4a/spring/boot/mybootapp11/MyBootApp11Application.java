9015aa146a2407547882300fed5afd25ca0c1ded
木村 光希 <180012@jc-21.jp>
2021-06-15 13:07:14 +0900

    owari

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp11/MyBootApp11Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.mybootapp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApp11Application.class, args);
+    }
+
+}
